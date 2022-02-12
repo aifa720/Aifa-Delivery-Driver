4ca8ef9182e44a44195e167acfdbaae51e439919
Maryam Aifa <aifa720@morrisville.edu>
2022-02-12 14:07:19 -0500

    Add Assets To Project

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) 
+    void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("What was that?!");
     }
 }
